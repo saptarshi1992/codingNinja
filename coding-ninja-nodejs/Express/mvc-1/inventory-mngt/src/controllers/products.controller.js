6f0ab41300ba35ba3354c54f1240287653bec25f
saptarshi1992 <saptarshi34.sc@gmail.com>
2024-08-28 18:28:11 +0530

    fix the /mvc-1/problem-2 bug

--- coding-ninja-nodejs/Express/mvc-1/inventory-mngt/src/controllers/products.controller.js
+++ coding-ninja-nodejs/Express/mvc-1/inventory-mngt/src/controllers/products.controller.js
@@ -1,7 +1,10 @@
 const path = require('path');
-class productController{
+const ProductModel = require('../model/products.model.js');
+class productController {
     getProducts(req, res) {
-       return res.sendFile(path.join(path.resolve(), "src", "views", "products.html"));
- }
+        let products = ProductModel.get();
+        console.log(products);
+        return res.sendFile(path.join(path.resolve(), "src", "views", "products.html"));
+    }
 }
 module.exports = productController;
---
+++ coding-ninja-nodejs/Express/mvc-1/problem-2/src/controllers/product.controller.js
@@ -0,0 +1,11 @@
+// Import the necessary modules here
+import ProductModel from "../models/product.model.js";
+
+export default class ProductController {
+  getProducts = (req, res) => {
+    //  Write your code here
+    const product = new ProductModel();
+    res.get('/', product.fetchProducts);
+
+  };
+}
