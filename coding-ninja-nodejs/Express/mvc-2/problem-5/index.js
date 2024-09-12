38dc66097f48fc8424573f2880063fbe2356e62e
saptarshi1992 <saptarshi34.sc@gmail.com>
2024-09-12 17:05:50 +0530

    mvc-2/problem-5

--- coding-ninja-nodejs/Express/mvc-2/problem-5/index.js
+++ coding-ninja-nodejs/Express/mvc-2/problem-5/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { newUser } from "./user.controller.js";
+import { formValidationMiddleware } from "./middleware.js";
 
 // Please don't change the pre-written code
 // Import the necessary modules here
@@ -8,6 +9,6 @@ const app = express();
 app.use(express.json());
 
 //  Modify 'app.post("/new", newUser);' route handler to use the 'formValidationMiddleware'.
-app.post("/new", newUser);
+app.post("/new",formValidationMiddleware, newUser);
 
 export default app;
